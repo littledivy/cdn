2db6cd5ad66309f481825e475e8bbb12d91b7194
Divy Srivastava <dj.srivastava23@gmail,com>
2019-07-31 18:37:32 +0530

    Dashboard enchanced with smooth graphs

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const debug = require('debug')('myapp:server');
 const path = require('path');
 const multer = require('multer');
 const logger = require('morgan');
-const serveIndex = require('serve-index')
+const serveIndex = require('serve-index');
+const os = require('os-utils');
+const nativeOS = require('os');
+const http = require('http');
+const io = require('socket.io');
+ 
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -18,29 +23,43 @@ var storage = multer.diskStorage({
 //will be using this for uplading
 const upload = multer({ storage: storage });
 
-//get the router
-//const userRouter =require('./routes/user.route');
-
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 app.use(logger('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(express.static('public'));
+
 app.use('/ftp', express.static('public'), serveIndex('public', {'icons': true}));
 
 app.get('/', function(req,res) {
-    return res.send("hello from my app express server!")
+        var stats = {}
+        stats.uptime = nativeOS.uptime()
+        stats.freemem = nativeOS.freemem()
+        console.log(stats)
+        return res.render('index',{
+            stats:stats
+        });
 })
 
-app.post('/testUpload', upload.single('file'), function(req,res) {
+app.post('/upload', upload.single('file'), function(req,res) {
     debug(req.file);
     console.log('storage location is ', req.hostname +'/' + req.file.path);
     return res.send(req.file);
 })
 
-//if end point is /users/, use the router.
-//app.use('/users', userRouter);
+var server = http.createServer(app);
+var sio = io(server); 
+
+sio.on("connection", function(socket) {
+   socket.on('gimme_cpu_usage', function(client) {
+        os.cpuUsage(function(v){
+            socket.emit('cpu_usage',v);
+        });
+   })
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
     debug('Server is up and running on port ', port);
 })
